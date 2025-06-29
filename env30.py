b4da58dd7e70aa1b9e4a82b2d2fced8332d8c907
sec-gatra <akhiragatra4@gmail.com>
2025-06-29 19:58:27 +0700

    Update env30.py

--- env30.py
+++ env30.py
@@ -6,7 +6,7 @@ class GameState:
     def __init__(self, nodes, p_max, area_size=(20, 20)):
         self.nodes = nodes
         self.p_max = p_max
-        self.gamma = 0.01
+        self.gamma = 0.99
         self.beta = 1
         self.noise_power = 0.01
         self.area_size = area_size
