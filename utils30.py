4ede679084c2526c9cdde25f231065d920f236bc
sec-gatra <akhiragatra4@gmail.com>
2025-06-30 06:35:18 +0700

    Update utils30.py

--- utils30.py
+++ utils30.py
@@ -69,7 +69,7 @@ class Q_Critic(nn.Module):
         return q
 
 def evaluate_policy(channel_gain, state, env, agent, turns=1):
-    env = GameState(30,15)
+    env = GameState(30,7)
     total_scores = 0
     total_scores_rand = 0 
     total_EE = 0
