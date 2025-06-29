6d36f894718dec37fae494a7e37d2f57e11fe697
sec-gatra <akhiragatra4@gmail.com>
2025-06-29 21:55:50 +0700

    Update env30.py

--- env30.py
+++ env30.py
@@ -71,7 +71,7 @@ class GameState:
         penalty_power   = 0.8 * power_violation
 
         # Reward: throughput minus penalties
-        reward = total_rate - penalty_rate# - penalty_power
+        reward = total_rate - penalty_rate # - penalty_power
         
         # Condition 1: Budget exceeded
         fail_power = total_daya > self.p_max
@@ -118,7 +118,7 @@ class GameState:
         'total_power': float(np.sum(power))
         }
 
-        reward = -np.sum(data_rate_constraint) + EE - 5*self.step_function(total_daya-self.p_max)
+        #reward = -np.sum(data_rate_constraint) + EE - 5*self.step_function(total_daya-self.p_max)
         obs = np.concatenate([self.norm(next_channel_gain).ravel(),self.norm(next_intr).ravel(),self.norm(power)])
         return obs.astype(np.float32), float(reward), dw,False, info
     def norm(self,x):
