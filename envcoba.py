4827a1f9c4205f8174e74864fe42cd8beaec76a0
sec-gatra <akhiragatra4@gmail.com>
2025-07-01 20:54:15 +0700

    Update envcoba.py

--- envcoba.py
+++ envcoba.py
@@ -75,7 +75,7 @@ class GameState:
         penalty_power   = 0.1 * power_violation
 
         # Reward: throughput minus penalties
-        reward = total_rate - penalty_rate #- penalty_power
+        reward = EE + total_rate - penalty_rate #- penalty_power
         # Final done flag for “dead/win”
         dw = bool(fail_power)
 
