e89f13273ac08190e874ee36fa581285092e2ad9
sec-gatra <akhiragatra4@gmail.com>
2025-07-02 14:05:42 +0700

    Update maincoba.py

--- maincoba.py
+++ maincoba.py
@@ -300,7 +300,7 @@ def main():
             ax4.plot(x, y, label=f'Node {idx}')
 
         # Garis vertikal untuk R_min
-        R_min = 0.152
+        R_min = 0.12
         ax4.axvline(R_min, color='red', linestyle='--', label=f'R_min = {R_min}')
 
         ax4.set_xlabel('Data Rate')
@@ -322,7 +322,7 @@ def main():
         ax5.plot(x_dr_rand, y_dr_rand, label='Random (All Nodes)', linestyle='--')
 
         # Tambahkan garis vertikal R_min
-        R_min = 0.152  # Ganti sesuai kebutuhan
+        R_min = 0.12  # Ganti sesuai kebutuhan
         ax5.axvline(R_min, color='red', linestyle='--', label=f'R_min = {R_min}')
 
         # Tambahkan panah horizontal untuk menunjukkan gap di CDF 0.5
