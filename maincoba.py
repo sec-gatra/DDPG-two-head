6ea3a9e24ac8b0bfb97ff150dc444c4db9205189
sec-gatra <akhiragatra4@gmail.com>
2025-07-05 17:57:38 +0700

    Update maincoba.py

--- maincoba.py
+++ maincoba.py
@@ -252,7 +252,16 @@ def main():
         if opt.write:
             writer.add_figure('CDF Data Rate Sistem', fig5, global_step=st)
             plt.close(fig5)
-
+        print(len(EE_DDPG))
+        print(len(EE_RAND))
+        print(len(data_rate_1))
+        print(len(data_rate_4))
+        print(len(data_rate_7))
+        print(len(data_rate_10))
+        print(len(POWER_DDPG))
+        print(len(POWER_RAND))
+        print(len(ALL_DATARATES))
+        print(len(ALL_DATARATES_RAND))
         # Buat dataframe
         df = pd.DataFrame({
             'EE_DDPG': EE_DDPG,
