ef7a86dbff6348c3a5b75608a4d37f22a63fe784
sec-gatra <akhiragatra4@gmail.com>
2025-07-08 10:54:20 +0700

    Update maincoba.py

--- maincoba.py
+++ maincoba.py
@@ -378,7 +378,7 @@ def main():
                     writer.add_scalar('reward_train', result['reward_train'], global_step=total_steps)
                     writer.add_scalar('reward training ddpg', result_reward, global_step=total_steps)
                     if total_steps == opt.Max_train_steps :
-                        for i in range(20000):
+                        for i in range(60000):
                             if i % 2000 == 0
                                 loc_extend= env.generate_positions() #lokasi untuk s_t
                                 channel_gain_extend=env.generate_channel_gain(loc_extend) #channel gain untuk s_t
