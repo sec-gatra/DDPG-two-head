7404ad9ce474931adc1b541fdce128ea8c9e1e40
sec-gatra <akhiragatra4@gmail.com>
2025-07-11 14:40:12 +0700

    Update env30.py

--- env30.py
+++ env30.py
@@ -104,7 +104,7 @@ class GameState:
 
         #reward = -np.sum(data_rate_constraint) + EE - 5*self.step_function(total_daya-self.p_max)
         #obs = np.concatenate([self.norm(next_channel_gain).ravel(),self.norm(next_intr).ravel(),self.norm(power)])
-        obs = ([self.norm(next_channel_gain).ravel()])
+        obs = (self.norm(next_channel_gain).ravel())
         return np.array(obs).astype(np.float32), float(reward), dw, False, info
     def norm(self,x):
         x = np.maximum(x, 1e-10) # aslinya kagak ada
