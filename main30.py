263d97293a3f1bdcd9e98c8af47fe9c03ba6a3cc
sec-gatra <akhiragatra4@gmail.com>
2025-07-12 20:21:17 +0700

    Update main30.py

--- main30.py
+++ main30.py
@@ -101,7 +101,7 @@ def main():
         for i in range((3000)):
         #for i in range(len(channel_gains_from_csv1)):
                             st+=1
-                            #loc_eval= env.generate_positions() #lokasi untuk s_t
+                            loc_eval= env.generate_positions() #lokasi untuk s_t
                             channel_gain_eval=env.generate_channel_gain(loc_eval) #channel gain untuk s_t
                             #channel_gain_eval = channel_gains_from_csv1[i]
                             state_eval,a_prev,inf=eval_env.reset(channel_gain_eval)
