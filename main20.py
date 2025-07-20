14d40565519842162b68d2c928c74ea2d74c4a7b
sec-gatra <akhiragatra4@gmail.com>
2025-07-20 19:04:58 +0700

    Update main20.py

--- main20.py
+++ main20.py
@@ -99,12 +99,12 @@ def main():
         rate_lolos=[]
         channel_gains_from_csv1 = np.load('channel_gains_from_csv.npy', allow_pickle=True)
         #channel_gains_from_csv2 = np.load('channel_gains_from_csv2.npy', allow_pickle=True)
-        #for i in range(len(channel_gains_from_csv1)):
-        for i in range(3000):
+        for i in range(len(channel_gains_from_csv1)):
+        #for i in range(3000):
                             st+=1
                             loc_eval= env.generate_positions() #lokasi untuk s_t
-                            #channel_gain_eval = channel_gains_from_csv1[i]
-                            channel_gain_eval=env.generate_channel_gain(loc_eval) #channel gain untuk s_t
+                            channel_gain_eval = channel_gains_from_csv1[i]
+                            #channel_gain_eval=env.generate_channel_gain(loc_eval) #channel gain untuk s_t
                             state_eval,inf=eval_env.reset(channel_gain_eval)
                             state_eval = np.array(state_eval, dtype=np.float32)
                             result1 = evaluate_policy(channel_gain_eval,state_eval,eval_env, agent, turns=1)
