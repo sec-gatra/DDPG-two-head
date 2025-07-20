46aa80bb3fdfbda200c17b059f05e0df64292342
sec-gatra <akhiragatra4@gmail.com>
2025-07-20 23:21:38 +0700

    Update env30.py

--- env30.py
+++ env30.py
@@ -90,7 +90,7 @@ class GameState:
         # Reward formula dinamis
         #reward = EE - k_dynamic * penalty_rate - beta * total_daya +  gammas*total_rate #- 10 * fairness_penalty
         #reward = EE - 5*rate_violation - np.sum(power)
-        reward = 10*EE - 10*rate_violation - 15*np.sum(power) + 10*total_rate
+        reward = 30*EE - 10*rate_violation - 15*np.sum(power) + 10*total_rate
         #reward = EE - k_dynamic * penalty_rate - beta * total_daya +  gammas*total_rate #- 10 * fairness_penalty
         #reward = alpha*np.log(EE) - beta*rate_violation - zeta*np.sum(penalty_power)
 
