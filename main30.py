c15cf2f212a5f266064d49c69d103d85529e83fe
sec-gatra <akhiragatra4@gmail.com>
2025-07-20 14:37:25 +0700

    Update main30.py

--- main30.py
+++ main30.py
@@ -98,7 +98,7 @@ def main():
         st=0
         rate_lolos=[]
         #channel_gains_from_csv1 = np.load('channel_gains_from_csv.npy', allow_pickle=True)
-        for i in range((300)):
+        for i in range((3000)):
         #for i in range(len(channel_gains_from_csv1)):
                             print(i)
                             st+=1
