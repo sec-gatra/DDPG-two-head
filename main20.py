908f78589c77c9bb9c1439f643c0344fb4e9d826
sec-gatra <akhiragatra4@gmail.com>
2025-07-20 20:59:59 +0700

    Update main20.py

--- main20.py
+++ main20.py
@@ -104,6 +104,7 @@ def main():
                             st+=1
                             loc_eval= env.generate_positions() #lokasi untuk s_t
                             channel_gain_eval = channel_gains_from_csv1[i]
+                            print(channel_gain_eval)
                             #channel_gain_eval=env.generate_channel_gain(loc_eval) #channel gain untuk s_t
                             state_eval,inf=eval_env.reset(channel_gain_eval)
                             state_eval = np.array(state_eval, dtype=np.float32)
