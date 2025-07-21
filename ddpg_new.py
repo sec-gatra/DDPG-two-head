c26cf7493a6e0e96803aade561288389f35df4d3
sec-gatra <akhiragatra4@gmail.com>
2025-07-21 20:12:19 +0700

    Update ddpg_new.py

--- ddpg_new.py
+++ ddpg_new.py
@@ -7,97 +7,73 @@ from torch.nn.utils import clip_grad_norm_
 
 class DDPG_agent():
 	def __init__(self, **kwargs):
-		# Init hyperparameters for agent, just like "self.gamma = opt.gamma, self.lambd = opt.lambd, ..."
-		self.__dict__.update(kwargs)
-		self.tau = 0.005
-
-		self.actor = Actor(self.state_dim, self.action_dim, self.net_width, self.max_action).to(self.dvc)
-		self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.a_lr)
-		self.actor_target = copy.deepcopy(self.actor)
-
-		self.q_critic = Q_Critic(self.state_dim, self.action_dim, self.net_width).to(self.dvc)
-		self.q_critic_optimizer = torch.optim.Adam(self.q_critic.parameters(), lr=self.c_lr)
-		self.q_critic_target = copy.deepcopy(self.q_critic)
-
-		self.replay_buffer = ReplayBuffer(self.state_dim, self.action_dim, max_size=int(160000), dvc=self.dvc)
-		
-	def select_action(self, state, deterministic):
-		with torch.no_grad():
-			state=np.array(state, dtype=np.float32)
-			
-			state = torch.FloatTensor(state[np.newaxis, :]).to(self.dvc)  # from [x,x,...,x] to [[x,x,...,x]]
-			a = self.actor(state).cpu().numpy()[0] # from [[x,x,...,x]] to [x,x,...,x]
-			if deterministic:
-				return a
-			else:
-				noise = np.random.normal(0, self.max_action * self.noise, size=self.action_dim)
-				#return (a + noise).clip(0, self.max_action) ini di komen karena katanya udah di batesin di softmax
-				return np.clip(a + noise, 0, self.max_action)
-
+		   # Actor
+	    self.actor = Actor(self.state_dim, self.action_dim, self.net_width, self.max_action).to(self.dvc)
+	    self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.a_lr)
+	    self.actor_scheduler  = torch.optim.lr_scheduler.ExponentialLR(self.actor_optimizer, gamma=0.995)
+	    self.actor_target = copy.deepcopy(self.actor)
+	
+	    # Critic
+	    self.q_critic = Q_Critic(self.state_dim, self.action_dim, self.net_width).to(self.dvc)
+	    self.q_critic_optimizer = torch.optim.Adam(self.q_critic.parameters(), lr=self.c_lr)
+	    self.critic_scheduler    = torch.optim.lr_scheduler.ExponentialLR(self.q_critic_optimizer, gamma=0.995)
+	    self.q_critic_target = copy.deepcopy(self.q_critic)
+	
+	    # Replay
+	    self.replay_buffer = ReplayBuffer(self.state_dim, self.action_dim, max_size=int(500e3), dvc=self.dvc)
+	    self.noise_proc    = OUNoise(self.action_dim)
+	
+	def select_action(self, state, deterministic=False):
+	    state = torch.FloatTensor(np.array(state, dtype=np.float32)[None]).to(self.dvc)
+	    a = self.actor(state).cpu().numpy()[0]
+	    if deterministic:
+	        return a
+	    noise = self.noise_proc()
+	    return np.clip(a + noise, 0, self.max_action)
+	
 	def train(self):
-		# Compute the target Q
-		with torch.no_grad():
-			s, a, r, s_next, dw = self.replay_buffer.sample(self.batch_size)
-			target_a_next = self.actor_target(s_next)
-			#mask = (~dw).float().unsqueeze(1)
-			target_Q= self.q_critic_target(s_next, target_a_next)
-			#target_Q = r + self.gamma * mask * target_Q #ini ditambahin dw biar bagus kata gpt
-			#target_Q = r + self.gamma * target_Q  #dw: die or win
-			target_Q = r + (~dw) * self.gamma * target_Q
-
-		# Get current Q estimates
-		current_Q = self.q_critic(s, a)
-
-			# Compute critic loss
-		q_loss = F.mse_loss(current_Q, target_Q)
-
-			# Optimize the q_critic
-		self.q_critic_optimizer.zero_grad()
-		q_loss.backward()
-
-			# Gradient clipping for critic
-		clip_grad_norm_(self.q_critic.parameters(), max_norm=1.0)
-
-		self.q_critic_optimizer.step()
-
-				# Update the Actor
-		self.actor_optimizer.zero_grad()
-		
-		# Matikan grad pada critic
-		for p in self.q_critic.parameters():
-		    p.requires_grad_(False)
-		a_loss = -self.q_critic(s, self.actor(s)).mean()
-		# Backprop dari actor loss
-		a_loss.backward()
-		
-		# Clip grad pada actor
-		clip_grad_norm_(self.actor.parameters(), max_norm=1.0)
-		
-		# Update actor
-		self.actor_optimizer.step()
-		
-		# Hidupkan lagi grad pada critic
-		for p in self.q_critic.parameters():
-		    p.requires_grad_(True)
-
-		# Update the frozen target models
-		with torch.no_grad():
-			for param, target_param in zip(self.q_critic.parameters(), self.q_critic_target.parameters()):
-				target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
-
-			for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
-				target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
-		return a_loss.item(), q_loss.item()
-
-	def save(self,EnvName, timestep):
-		torch.save(self.actor.state_dict(), "./model/{}_actor{}.pth".format(EnvName,timestep))
-		torch.save(self.q_critic.state_dict(), "./model/{}_q_critic{}.pth".format(EnvName,timestep))
-
-	def load(self,EnvName, timestep):
-		self.actor.load_state_dict(torch.load("./model/{}_actor{}.pth".format(EnvName, timestep), map_location=self.dvc))
-		self.q_critic.load_state_dict(torch.load("./model/{}_q_critic{}.pth".format(EnvName, timestep), map_location=self.dvc))
-
-
+	    # Sample
+	    s, a, r, s_next, dw = self.replay_buffer.sample(self.batch_size)
+	    with torch.no_grad():
+	        # Target smoothing
+	        target_a = self.actor_target(s_next)
+	        noise = (torch.randn_like(target_a) * 0.2).clamp(-0.5, 0.5)
+	        target_a = (target_a + noise).clamp(0, self.max_action)
+	        target_Q = self.q_critic_target(s_next, target_a)
+	        target_Q = r + (~dw) * self.gamma * target_Q
+	    # Critic update
+	    current_Q = self.q_critic(s, a)
+	    q_loss = F.mse_loss(current_Q, target_Q)
+	    self.q_critic_optimizer.zero_grad()
+	    q_loss.backward()
+	    clip_grad_norm_(self.q_critic.parameters(), max_norm=0.5)
+	    self.q_critic_optimizer.step()
+	    # Actor update
+	    for p in self.q_critic.parameters(): p.requires_grad_(False)
+	    a_loss = -self.q_critic(s, self.actor(s)).mean()
+	    self.actor_optimizer.zero_grad()
+	    a_loss.backward()
+	    clip_grad_norm_(self.actor.parameters(), max_norm=0.5)
+	    self.actor_optimizer.step()
+	    for p in self.q_critic.parameters(): p.requires_grad_(True)
+	    # Soft update
+	    with torch.no_grad():
+	        for p, tp in zip(self.q_critic.parameters(), self.q_critic_target.parameters()):
+	            tp.data.mul_(1 - self.tau); tp.data.add_(self.tau * p.data)
+	        for p, tp in zip(self.actor.parameters(), self.actor_target.parameters()):
+	            tp.data.mul_(1 - self.tau); tp.data.add_(self.tau * p.data)
+	    # Schedulers
+	    self.actor_scheduler.step()
+	    self.critic_scheduler.step()
+	    return a_loss.item(), q_loss.item()
+	
+	def save(self, EnvName, timestep):
+	    torch.save(self.actor.state_dict(), f"./model/{EnvName}_actor{timestep}.pth")
+	    torch.save(self.q_critic.state_dict(), f"./model/{EnvName}_critic{timestep}.pth")
+	
+	def load(self, EnvName, timestep):
+	    self.actor.load_state_dict(torch.load(f"./model/{EnvName}_actor{timestep}.pth", map_location=self.dvc))
+	    self.q_critic.load_state_dict(torch.load(f"./model/{EnvName}_critic{timestep}.pth", map_location=self.dvc))
 class ReplayBuffer():
 	def __init__(self, state_dim, action_dim, max_size, dvc):
 		self.max_size = max_size
