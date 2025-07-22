a87204050e865efdd6e3d1c58acb911356f7b5b3
sec-gatra <akhiragatra4@gmail.com>
2025-07-23 00:45:51 +0700

    Update main30.py

--- main30.py
+++ main30.py
@@ -27,8 +27,8 @@ parser.add_argument('--eval_interval', type=int, default=2000, help='Model evalu
 
 parser.add_argument('--gamma', type=float, default=0.99, help='Discounted Factor')
 parser.add_argument('--net_width', type=int, default=1024, help='Hidden net width, s_dim-400-300-a_dim')
-parser.add_argument('--a_lr', type=float, default=1e-5, help='Learning rate of actor') # 1e-4
-parser.add_argument('--c_lr', type=float, default=1e-6, help='Learning rate of critic') # 1e-5 bagus
+parser.add_argument('--a_lr', type=float, default=1e-3, help='Learning rate of actor') # 1e-4
+parser.add_argument('--c_lr', type=float, default=1e-4, help='Learning rate of critic') # 1e-5 bagus
 parser.add_argument('--batch_size', type=int, default=128, help='batch_size of training')
 parser.add_argument('--random_steps', type=int, default=5000, help='random steps before trianing')
 parser.add_argument('--noise', type=float, default=0.1, help='exploring noise') #aslinya 0.1
@@ -215,8 +215,8 @@ def main():
         while total_steps < opt.Max_train_steps: # ini loop episode. Jadi total episode adalah Max_train_steps/200
             lr_steps+=1
             if lr_steps==sepertiga_eps :
-                opt.a_lr=0.3 * opt.a_lr
-                opt.c_lr=0.3 * opt.c_lr
+                opt.a_lr=0.8 * opt.a_lr
+                opt.c_lr=0.8 * opt.c_lr
                 lr_steps=0
             loc= env.generate_positions() #lokasi untuk s_t
             channel_gain=env.generate_channel_gain(loc) #channel gain untuk s_t
