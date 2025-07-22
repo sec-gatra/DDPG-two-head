78948dd22571c8128e5e93083f8a0c6425fb6f58
sec-gatra <akhiragatra4@gmail.com>
2025-07-23 00:47:29 +0700

    Update main30.py

--- main30.py
+++ main30.py
@@ -279,6 +279,7 @@ def main():
                     print(f'data rate lolos : {result["data_rate_lolos"]}')
                     print(f'rate violation : {result["rate_violation"]}')
                     print(f'EE: {result["avg_EE"]}')
+                    print(f'Actor rate : {opt.a_lr}, critic rate : {opt.c_lr}')
                     print(f'step : {total_steps}')
                     #print(f'percentase rate lolos: {result["total_rate_lolos"]}')
                     if result['avg_EE'] >= 30 and result['data_rate_lolos']>=0.7*env.nodes :
