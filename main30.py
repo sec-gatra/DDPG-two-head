93afa49f4b98fcc079b9dd880a527c9444ff7ab8
sec-gatra <akhiragatra4@gmail.com>
2025-07-23 11:35:01 +0700

    Update main30.py

--- main30.py
+++ main30.py
@@ -29,8 +29,8 @@ parser.add_argument('--gamma', type=float, default=0.99, help='Discounted Factor
 parser.add_argument('--net_width', type=int, default=1024, help='Hidden net width, s_dim-400-300-a_dim')
 parser.add_argument('--a_lr', type=float, default=1e-5, help='Learning rate of actor') # 1e-4
 parser.add_argument('--c_lr', type=float, default=1e-6, help='Learning rate of critic') # 1e-5 bagus
-parser.add_argument('--batch_size', type=int, default=128, help='batch_size of training')
-parser.add_argument('--random_steps', type=int, default=5000, help='random steps before trianing')
+parser.add_argument('--batch_size', type=int, default=10, help='batch_size of training')
+parser.add_argument('--random_steps', type=int, default=10, help='random steps before trianing')
 parser.add_argument('--noise', type=float, default=0.1, help='exploring noise') #aslinya 0.1
 opt = parser.parse_args()
 opt.dvc = torch.device(opt.dvc) # from str to torch.device
